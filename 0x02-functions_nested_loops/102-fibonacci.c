260441c42abc4d505d5ba91000836110f5fc4b73
Mvyvdv <mayyadaibnouf@gmail.com>
2022-12-13 10:56:09 -0800

    a program that prints the first 50 Fibonacci numbers

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -16,11 +16,11 @@ int main(void)
 	{
 		if (j != 20365011074)
 		{
-			printf("%d, ", j);
+			printf("%ld, ", j);
 		}
 		else
 		{
-			printf("%d\n", j);
+			printf("%ld\n", j);
 		}
 		l = j + k;
 		j = k;

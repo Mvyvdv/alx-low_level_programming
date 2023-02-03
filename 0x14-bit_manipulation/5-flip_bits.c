4eff6a90b3e449370261cdd4642b93e5d36eb84d
Mvyvdv <mayyadaibnouf@gmail.com>
2023-02-03 10:51:10 -0800

    Task 5

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -13,7 +13,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 
 	for (i = 0; i < j; i++)
 	{
-		if ((m & !) != (n & 1))
+		if ((m & 1) != (n & 1))
 			nflips += 1;
 		n = n >> 1;
 		m = m >> 1;
